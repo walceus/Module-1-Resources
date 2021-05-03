00bd9cdce0f5ad2e9f3740eb30ef97bcd5219065
walceus <widalceus@gmail.com>
2021-05-03 15:07:35 -0400

    Update debugging.js

--- week1/d1-expressions/code-it-out/debugging.js
+++ week1/d1-expressions/code-it-out/debugging.js
@@ -1,8 +1,8 @@
 /* 
 What is Debugging?
-
+When you find an error in your code and you go through the process of fixing it so the code works.
 
 How often will you be wrong, and need to debug?
-
+As long you code, mistakes will be made and debugged.
 */
 
